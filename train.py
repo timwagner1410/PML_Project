460ec72bb968b667b25c654ead5e21e35a25d9c9
timwagner1410 <timwagner1410@gmail.com>
2025-01-16 14:18:35 +0100

    some adjustments

--- train.py
+++ train.py
@@ -2,15 +2,20 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from agent import SnakeEnv
 
+
+train_mode: bool = False
+
 # Create the environment
-env = DummyVecEnv([lambda: SnakeEnv(show=False)])
+env = DummyVecEnv([lambda: SnakeEnv(show=not train_mode)])
 
 # Create the PPO model
 model = PPO("MlpPolicy", env, verbose=1)
 
-model.learn(total_timesteps=100000)
-model.save("ppo_snake_4")
-# model = PPO.load("ppo_snake_4")
+if train_mode:
+    model.learn(total_timesteps=100000)
+    model.save("ppo_snake_4")
+else:
+    model = PPO.load("ppo_snake_4")
 
 # Test the trained model
 obs = env.reset()
