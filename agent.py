fa88204b09370039d46657fd14b867fbdd74a091
BinPatryk <patryksylerix@web.de>
2025-01-22 20:07:35 +0100

    agent observation space defined and fixed movement

--- agent.py
+++ agent.py
@@ -8,22 +8,29 @@ from game import Game
 class SnakeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self):
+    def __init__(self, show: bool = True):
         super(SnakeEnv, self).__init__()
         self.game = Game()
         self.action_space = spaces.Discrete(3)  # 1 = same dir; 2 = left; 3 = right
+        self.show_ui = show
+        self.ai_score = 0
+        self.bot_score = 0
 
         # Define the observation space
-        self.observation_space = spaces.Box(low=-1, high=3, shape=(9,), dtype=np.int32)
+        self.observation_space = spaces.Box(low=-1, high=3, shape=(self.game.h // self.game.block_size +2, self.game.w // self.game.block_size +2), dtype=np.int32)
 
     def reset(self, seed=None):
         self.game = Game()
         return self._get_observation(), {}
 
-    def step(self, action):
-        if action == 2:  # move direction to left
+
+    def step(self, action: object) -> object:
+
+        if action == 1:  # maintain current direction
+            pass
+        elif action == 2:  # turn left
             self.game.snake_1.direction = (-self.game.snake_1.direction[1], self.game.snake_1.direction[0])
-        elif action == 3:  # move direction to right
+        elif action == 0:  # turn right
             self.game.snake_1.direction = (self.game.snake_1.direction[1], -self.game.snake_1.direction[0])
 
         self.game.play_step()
@@ -31,17 +38,27 @@ class SnakeEnv(gym.Env):
         done = abs(self.game.game_state) == 1
 
         # reward function for snake 1
+        """
+            2: Snake of Player 2 gets Apple
+            1: Snake of Player 2 collides
+            0: Nothing happens
+            -1: Snake of Player 1 collides
+            -2: Snake of Player 1 gets Apple
+        """
         rewards = {
-            2: -0.5,
-            1: 10,
-            0: 0.1,
-            -1: -20,
-            -2: 1
+            2: 0,
+            1: 50,
+            0: 1,
+            -1: -30,
+            -2: 8
         }
 
         reward = rewards[self.game.game_state]
 
-        # print(f"Game State: {self.game.game_state}, Reward: {reward}")
+        if self.game.game_state == 1:
+            self.ai_score += 1
+        elif self.game.game_state == -1:
+            self.bot_score += 1
 
         terminated = done
         truncated = False  # Set this to True if you have a time limit or other truncation condition
@@ -53,26 +70,29 @@ class SnakeEnv(gym.Env):
         time.sleep(0.05)  # Add a delay of 0.1 seconds
 
     def _get_observation(self):
-        head_x, head_y = self.game.snake_1.body[0]
-        surrounding = [
-            (head_x - 1, head_y - 1), (head_x, head_y - 1), (head_x + 1, head_y - 1),
-            (head_x - 1, head_y), (head_x, head_y), (head_x + 1, head_y),
-            (head_x - 1, head_y + 1), (head_x, head_y + 1), (head_x + 1, head_y + 1)
-        ]
-        observation = []
-        for cell in surrounding:
-            if cell in self.game.snake_1.body:
-                observation.append(1)  # AI snake body
-            elif cell in self.game.snake_2.body:
-                observation.append(2)  # Player snake body
-            elif cell == (self.game.apple.x // self.game.block_size, self.game.apple.y // self.game.block_size):
-                observation.append(3)  # Apple
-            elif 0 <= cell[0] < self.game.w // self.game.block_size and 0 <= cell[1] < self.game.h // self.game.block_size:
-                observation.append(0)  # Empty space
-            else:
-                observation.append(-1)  # Wall
+        if self.show_ui:
+            self.render()
 
-        return np.array(observation)
+        observation = np.zeros((self.game.h // self.game.block_size + 2, self.game.w // self.game.block_size + 2),
+                               dtype=np.int32)
+
+        for y in range(self.game.h // self.game.block_size + 2):
+            for x in range(self.game.w // self.game.block_size + 2):
+                if x == 0 or x == (self.game.w // self.game.block_size) + 1 or y == 0 or y == (
+                        self.game.h // self.game.block_size) + 1:
+                    observation[y, x] = -1  # Wall
+                else:
+                    cell = (x - 1, y - 1)
+                    if cell in self.game.snake_1.body:
+                        observation[y, x] = 1  # AI snake body
+                    elif cell in self.game.snake_2.body:
+                        observation[y, x] = 2  # Player snake body
+                    elif cell == (self.game.apple.x // self.game.block_size, self.game.apple.y // self.game.block_size):
+                        observation[y, x] = 3  # Apple
+                    else:
+                        observation[y, x] = 0
+
+        return observation
 
 if __name__ == '__main__':
     env = SnakeEnv()
